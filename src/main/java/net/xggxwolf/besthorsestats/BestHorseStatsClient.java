97dce1e56b1a59676f72cd5034d416de90b9d07e
XggXWolf <cetinmert058@gmail.com>
2024-04-18 12:27:11 +0300

    Initial Commit

---
+++ src/main/java/net/xggxwolf/besthorsestats/BestHorseStatsClient.java
@@ -0,0 +1,10 @@
+package net.xggxwolf.besthorsestats;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BestHorseStatsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/xggxwolf/besthorsestats/BestHorseStatsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.xggxwolf.besthorsestats;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BestHorseStatsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
