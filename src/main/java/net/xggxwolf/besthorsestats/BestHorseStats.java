58cc0eb9fbdbf5436dc6c95a7f67727af918fb19
XggXWolf <cetinmert058@gmail.com>
2024-04-19 16:50:37 +0300

    Hooked FindBestHorse to keybind, reformatted code

--- src/main/java/net/xggxwolf/besthorsestats/BestHorseStats.java
+++ src/main/java/net/xggxwolf/besthorsestats/BestHorseStats.java
@@ -1,36 +1,16 @@
 package net.xggxwolf.besthorsestats;
-import java.util.List;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.passive.HorseEntity;
-import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResult;
-import net.minecraft.text.Text;
-import java.util.concurrent.*;
-
-
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.world.World;
 import net.xggxwolf.besthorsestats.utils.FindBestHorse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BestHorseStats implements ClientModInitializer {
-	public static final String MOD_ID = "besthorsestats";
+    public static final String MOD_ID = "besthorsestats";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitializeClient() {
-		new FindBestHorse().register();
-	}
-
-
+    @Override
+    public void onInitializeClient() {
+        new FindBestHorse().register();
+    }
 }
--- src/main/java/net/xggxwolf/besthorsestats/utils/FindBestHorse.java
+++ src/main/java/net/xggxwolf/besthorsestats/utils/FindBestHorse.java
@@ -1,7 +1,11 @@
+
+// FindBestHorse.java
 package net.xggxwolf.besthorsestats.utils;
 
-import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.HorseEntity;
@@ -11,8 +15,10 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -20,16 +26,31 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class FindBestHorse {
+    public static final String MOD_ID = "besthorsestats";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-        public static final String MOD_ID = "besthorsestats";
-        public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    private final KeyBinding keyBinding;
 
-        public void register() {
-            UseEntityCallback.EVENT.register(this::onHorseRightClick);
-        }
+    public FindBestHorse() {
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.besthorsestats.findfastesthorse", // The translation key of the keybinding's name
+                InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                GLFW.GLFW_KEY_K, // The keycode of the key
+                "category.besthorsestats" // The translation key of the keybinding's category.
+        ));
+    }
 
-    private ActionResult onHorseRightClick(PlayerEntity player, World world, Hand hand, Entity entity, EntityHitResult hitResult) {
-        if (world.isClient && entity instanceof HorseEntity) {
+    public void register() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.wasPressed()) {
+                onKeyPress(client.player);
+            }
+        });
+    }
+
+    private void onKeyPress(PlayerEntity player) {
+        World world = player.getWorld();
+        if (world.isClient) {
             MinecraftClient minecraftClient = MinecraftClient.getInstance();
             int simulationDistance = minecraftClient.options.getSimulationDistance().getValue();
 
@@ -38,7 +59,6 @@ public class FindBestHorse {
                     player.getBoundingBox().expand(viewDistance),
                     (nearbyEntity) -> true);
 
-            HorseEntity clicked = (HorseEntity) entity;
             HorseEntity fastestHorse = null;
             double maxSpeed = 0;
 
@@ -50,19 +70,19 @@ public class FindBestHorse {
                 jumpHeight = nearbyHorse.getAttributeValue(EntityAttributes.HORSE_JUMP_STRENGTH);
                 health = nearbyHorse.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH);
 
-                if (movementSpeed > maxSpeed){
+                if (movementSpeed > maxSpeed) {
                     maxSpeed = movementSpeed;
                     fastestHorse = nearbyHorse;
                 }
             }
 
-            if(fastestHorse != null){
-                int x = Math.round((float)fastestHorse.getX());
-                int y = Math.round((float)fastestHorse.getY());
-                int z = Math.round((float)fastestHorse.getZ());
+            if (fastestHorse != null) {
+                int x = Math.round((float) fastestHorse.getX());
+                int y = Math.round((float) fastestHorse.getY());
+                int z = Math.round((float) fastestHorse.getZ());
                 double speed = fastestHorse.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) * 42.16;
                 jumpHeight = (
-                        - 0.1817584952 * Math.pow(jumpHeight, 3) +
+                        -0.1817584952 * Math.pow(jumpHeight, 3) +
                                 3.689713992 * Math.pow(jumpHeight, 2) +
                                 2.128599134 * jumpHeight - 0.343930367
                 ); // convert to blocks
@@ -73,8 +93,5 @@ public class FindBestHorse {
                 executorService.schedule(() -> player.sendMessage(Text.of(message), true), 100, TimeUnit.MILLISECONDS);
             }
         }
-
-        return ActionResult.PASS;
     }
-
 }
