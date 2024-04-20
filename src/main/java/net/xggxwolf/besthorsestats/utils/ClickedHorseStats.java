ed845c37635a27fd5dec95edfaae29cc5cb95788
XggXWolf <cetinmert058@gmail.com>
2024-04-20 14:55:11 +0300

    Added keybind to switch between healthiest, fastest and highest jump height horse, the mod is ready for its first release

--- src/main/java/net/xggxwolf/besthorsestats/utils/ClickedHorseStats.java
+++ src/main/java/net/xggxwolf/besthorsestats/utils/ClickedHorseStats.java
@@ -4,14 +4,21 @@ import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.HorseEntity;
+import net.minecraft.entity.passive.DonkeyEntity;
+import net.minecraft.entity.passive.MuleEntity;
+import net.minecraft.entity.passive.LlamaEntity;
+
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
+import net.minecraft.nbt.NbtCompound;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -26,6 +33,7 @@ public class ClickedHorseStats {
     }
 
     private ActionResult onHorseRightClick(PlayerEntity playerEntity, World world, Hand hand, Entity entity, EntityHitResult entityHitResult) {
+        // Horse stats
         if (world.isClient && entity instanceof HorseEntity clicked) {
             double maxSpeed = clicked.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) * 42.16;
             double jumpStrength = clicked.getAttributeValue(EntityAttributes.HORSE_JUMP_STRENGTH);
@@ -37,6 +45,43 @@ public class ClickedHorseStats {
             ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
             executorService.schedule(() -> playerEntity.sendMessage(Text.literal(message), true), 100, TimeUnit.MILLISECONDS);
         }
+        // Donkey stats
+        if (world.isClient && entity instanceof DonkeyEntity clicked) {
+            double maxSpeed = clicked.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) * 42.16;
+            double jumpStrength = clicked.getAttributeValue(EntityAttributes.HORSE_JUMP_STRENGTH);
+            double health = clicked.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH);
+            double jumpHeight = -0.1817584952 * Math.pow(jumpStrength, 3) + 3.689713992 * Math.pow(jumpStrength, 2) + 2.128599134 * jumpStrength - 0.343930367;
+
+            String message = String.format("Movement Speed: %.2f Blocks/sec, Jump Height: %.2f Blocks, Max Health : %.2f", maxSpeed, jumpHeight, health);
+
+            ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+            executorService.schedule(() -> playerEntity.sendMessage(Text.literal(message), true), 100, TimeUnit.MILLISECONDS);
+        }
+        // Mule stats
+        if (world.isClient && entity instanceof MuleEntity clicked) {
+            double maxSpeed = clicked.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) * 42.16;
+            double jumpStrength = clicked.getAttributeValue(EntityAttributes.HORSE_JUMP_STRENGTH);
+            double health = clicked.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH);
+            double jumpHeight = -0.1817584952 * Math.pow(jumpStrength, 3) + 3.689713992 * Math.pow(jumpStrength, 2) + 2.128599134 * jumpStrength - 0.343930367;
+
+            String message = String.format("Movement Speed: %.2f Blocks/sec, Jump Height: %.2f Blocks, Max Health : %.2f", maxSpeed, jumpHeight, health);
+
+            ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+            executorService.schedule(() -> playerEntity.sendMessage(Text.literal(message), true), 100, TimeUnit.MILLISECONDS);
+        }
+        // Llama stats
+        if (world.isClient && entity instanceof LlamaEntity clicked) {
+            double maxSpeed = clicked.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) * 42.16;
+            double health = clicked.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH);
+            NbtCompound nbt = clicked.writeNbt(new NbtCompound());
+            int invSpace = nbt.getInt("Strength") * 3;
+
+            String message = String.format("Movement Speed: %.2f Blocks/sec, Inventory Space: %d Items, Max Health : %.2f", maxSpeed, invSpace, health);
+
+            ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+            executorService.schedule(() -> playerEntity.sendMessage(Text.literal(message), true), 100, TimeUnit.MILLISECONDS);
+        }
+
         return ActionResult.PASS;
     }
 
--- src/main/java/net/xggxwolf/besthorsestats/utils/FindBestHorse.java
+++ src/main/java/net/xggxwolf/besthorsestats/utils/FindBestHorse.java
@@ -4,6 +4,7 @@ package net.xggxwolf.besthorsestats.utils;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -25,23 +26,54 @@ public class FindBestHorse {
     public static final String MOD_ID = "besthorsestats";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    private final KeyBinding keyBinding;
-
+    private final KeyBinding keyBinding1;
+    private final KeyBinding keyBinding2;
     public FindBestHorse() {
-        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.besthorsestats.findfastesthorse", // The translation key of the keybinding's name
+        keyBinding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Find Best Horse", // The translation key of the keybinding's name
                 InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
                 GLFW.GLFW_KEY_K, // The keycode of the key
-                "category.besthorsestats" // The translation key of the keybinding's category.
+                "Best Horse Stats" // The translation key of the keybinding's category.
+        ));
+
+        keyBinding2 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Switch Types", // The translation key of the keybinding's name
+                InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                GLFW.GLFW_KEY_J, // The keycode of the key
+                "Best Horse Stats" // The translation key of the keybinding's category.
         ));
     }
 
     public void register() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (keyBinding.wasPressed()) {
+            while (keyBinding1.wasPressed()) {
+                assert client.player != null;
                 onKeyPress(client.player);
             }
+            while (keyBinding2.wasPressed()) {
+                assert client.player != null;
+                onKeyPress2(client.player);
+            }
         });
+    }
+    int type = 0;
+    private void onKeyPress2(ClientPlayerEntity player) {
+        type = (type + 1) % 3;
+        String message = "Searching for ";
+        switch (type) {
+            case 0:
+                message += "Fastest Horse";
+                break;
+            case 1:
+                message += "Healthiest Horse";
+                break;
+            case 2:
+                message += "Horse with Highest Jump Height";
+                break;
+        }
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        String finalMessage = message;
+        executorService.schedule(() -> player.sendMessage(Text.literal(finalMessage), true), 100, TimeUnit.MILLISECONDS);
     }
 
     private void onKeyPress(PlayerEntity player) {
@@ -55,8 +87,13 @@ public class FindBestHorse {
                     player.getBoundingBox().expand(viewDistance),
                     (nearbyEntity) -> true);
 
-            HorseEntity fastestHorse = null;
+            HorseEntity lfHorse = null;
             double maxSpeed = 0;
+            double maxHealth = 0;
+            double maxJump = 0;
+
+            String lf = "";
+
 
             double jumpHeight = 0;
             double health = 0;
@@ -65,25 +102,41 @@ public class FindBestHorse {
                 double movementSpeed = nearbyHorse.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED);
                 jumpHeight = nearbyHorse.getAttributeValue(EntityAttributes.HORSE_JUMP_STRENGTH);
                 health = nearbyHorse.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH);
-
-                if (movementSpeed > maxSpeed) {
-                    maxSpeed = movementSpeed;
-                    fastestHorse = nearbyHorse;
+                switch (type) {
+                    case 0:
+                        if (movementSpeed > maxSpeed) {
+                            maxSpeed = movementSpeed;
+                            lfHorse = nearbyHorse;
+                            lf = "Fastest";
+                        }
+                        break;
+                    case 1:
+                        if (health > maxHealth) {
+                            maxHealth = health;
+                            lfHorse = nearbyHorse;
+                            lf = "Healthiest";
+                        }
+                        break;
+                    case 2:
+                        if (jumpHeight > maxJump) {
+                            maxJump = jumpHeight;
+                            lfHorse = nearbyHorse;
+                            lf = "Highest Jump";
+                        }
+                        break;
                 }
             }
 
-            if (fastestHorse != null) {
-                int x = Math.round((float) fastestHorse.getX());
-                int y = Math.round((float) fastestHorse.getY());
-                int z = Math.round((float) fastestHorse.getZ());
-                double speed = fastestHorse.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) * 42.16;
-                jumpHeight = (
-                        -0.1817584952 * Math.pow(jumpHeight, 3) +
-                                3.689713992 * Math.pow(jumpHeight, 2) +
-                                2.128599134 * jumpHeight - 0.343930367
-                ); // convert to blocks
+            if (lfHorse != null) {
+                int x = Math.round((float) lfHorse.getX());
+                int y = Math.round((float) lfHorse.getY());
+                int z = Math.round((float) lfHorse.getZ());
+                double speed = lfHorse.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) * 42.16;
+                double jump = lfHorse.getAttributeValue(EntityAttributes.HORSE_JUMP_STRENGTH);
+                double hp = lfHorse.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH);
+                jump = -0.1817584952 * Math.pow(jump, 3) + 3.689713992 * Math.pow(jump, 2) + 2.128599134 * jump - 0.343930367;
 
-                String message = String.format("Found Fastest Horse %d , %d , %d Movement Speed: %.2f Blocks/sec, Jump Height: %.2f, Max Health : %.2f", x, y, z, speed, jumpHeight, health);
+                String message = String.format("Found %s Horse at : %d , %d , %d : Movement Speed: %.2f m/s , Jump Height: %.2f, Health : %d", lf , x, y, z, speed, jump, (int)hp);
 
                 ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
                 executorService.schedule(() -> player.sendMessage(Text.literal(message), false), 100, TimeUnit.MILLISECONDS);
